326f4c8bcda84f5ec889bb6bdedb869532fe9db9
Julien Ouellet <JUOUE88@ulaval.ca>
2022-03-15 00:20:41 -0400

    fetch product from db and print in terminal

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ import serial
 from PySide6.QtWidgets import *
 from PySide6.QtGui import QCloseEvent
 import sys
+import csv
 
 
-class ArduinoControl(QWidget):
+class MainWindow(QWidget):
 
     def __init__(self):
         super().__init__()
         self.setup()
 
     def setup(self):
+
+        # Define upc to test button
+        btn_test_upc = QPushButton('Tester un code UPC', self)
+        btn_test_upc.clicked.connect(self.upcInputEvent)
+        btn_test_upc.move(100, 300)
+
         # Define quit button
         btn_quit = QPushButton('Quitter', self)
         btn_quit.clicked.connect(QApplication.instance().quit)
         btn_quit.resize(btn_quit.sizeHint())
         btn_quit.move(100, 100)
 
-        # Define upc to test button
-        btn_test_upc = QPushButton('Tester un code UPC', self)
-        btn_test_upc.clicked.connect(self.upcInputEvent)
-        btn_test_upc.move(300, 100)
 
         # Define window settings
         self.setGeometry(1280, 720, 1280, 720)
@@ -28,9 +31,10 @@ class ArduinoControl(QWidget):
         self.show()
 
     def upcInputEvent(self):
-        upc_num, result = QInputDialog.getInt(self, 'Input Dialog', "Entrer le numéro UPC du produit à tester : ")
+        upc_num, result = QInputDialog.getText(self, 'Input Dialog', "Entrer le numéro UPC du produit à tester : ")
         if result:
-            print(upc_num)
+            upc_num = int(upc_num)
+            return findProduct(upc_num)
 
     def closeEvent(self, event: QCloseEvent):
         reply = QMessageBox.question(self, 'Message', 'Are you sure you want to quit?',
@@ -42,9 +46,22 @@ class ArduinoControl(QWidget):
             event.ignore()
 
 
+def findProduct(value):
+    with open(r'DB/upcPrice.txt', 'r') as file:
+        reader = csv.reader(file)
+
+        for line in reader:
+            print("searching...")
+            if int(line[0]) == value:
+                upc_found = line
+                print(upc_found)
+                break
+    return upc_found
+
+
 def run():
     app = QApplication(sys.argv)
-    arduinoControl = ArduinoControl()
+    mainWindow = MainWindow()
     app.exec()
 
 
