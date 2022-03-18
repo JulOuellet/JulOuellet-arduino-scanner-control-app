00a3db782d8f7bc65149b90ab96b267a9712126e
Julien Ouellet <JUOUE88@ulaval.ca>
2022-03-17 23:46:19 -0400

    Minor bugfix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
 
-codeDeTest = "715067004297"
+codeDeTest = "074312840708"
 
 
 class MainWindow(QMainWindow):
