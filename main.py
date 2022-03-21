13a4a543f590425540aa98a9b34df73d26480f20
Julien Ouellet <julien.ouellet@live.ca>
2022-03-20 21:16:34 -0400

    main.py updates

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from PySide6.QtWidgets import *
 import interface
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
+app = QApplication(sys.argv)
 
-    window = interface.MainApp()
-    window.setWindowTitle('Arduino control app')
-    window.resize(750, 720)
-    window.show()
+window = interface.MainApp()
+window.setWindowTitle('Arduino control app')
+window.resize(750, 720)
+window.show()
 
-    sys.exit(app.exec())
+sys.exit(app.exec())
